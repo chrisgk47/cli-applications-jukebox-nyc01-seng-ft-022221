9c23e7bb48fdcc8b1d39990e83159f2e9c496a7e
Christopher Kim <chris.kim1990@gmail.com>
2021-02-20 20:07:27 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -5,3 +5,8 @@ def help
   puts /- play : lets you choose a song to play/
   puts /- exit : exits this program/
 end
+
+def list(array)
+  array.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end 
